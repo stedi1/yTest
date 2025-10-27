e597e596bb86cb6ad898fb823f4af5ffac445a60
Eduard <stedi.str@gmail.com>
2025-10-27 11:15:48 +0300

    v0.1.0

--- list/list.go
+++ list/list.go
@@ -1,6 +1,10 @@
 // list.go
 package list
 
+import (
+	"fmt"
+)
+
 func Min(list []int) int {
 	var min int
 	if len(list) < 1 {
@@ -13,4 +17,28 @@ func Min(list []int) int {
 		}
 	}
 	return min
+}
+
+func Mul(a, b []int) ([]int, error) {
+	if len(a) != len(b) {
+		return nil, fmt.Errorf("different length of slices")
+	}
+	result := make([]int, len(a))
+	for i, v := range a {
+		result[i] = v * b[i]
+	}
+	return result, nil
+}
+
+// напишите рекурсивную функцию GCD, которая вычисляет НОД
+// двух чисел по алгоритму Евклида
+func GCD(a, b int) int {
+	if a <= 0 || b <= 0 {
+		return 0
+	}
+	r := a % b
+	if r == 0 {
+		return b
+	}
+	return GCD(b, r)
 }
--- list/list_test.go
+++ list/list_test.go
@@ -1,6 +1,10 @@
 package list
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestMin(t *testing.T) {
 	// варианты последовательностей
@@ -19,5 +23,53 @@ func TestMin(t *testing.T) {
 				t.Errorf("get %d != want %d", Min(list[1:]), list[0])
 			}
 		})
+	}
+}
+
+func TestMul(t *testing.T) {
+	listA := [][]int{
+		{},
+		{3},
+		{-5, 6, 10},
+	}
+	listB := [][]int{
+		{},
+		{7},
+		{21, 0, 4},
+	}
+	// ожидаемые слайсы results[i] = list[i]*listB[i]
+	results := [][]int{
+		{},
+		{21},
+		{-105, 0, 40},
+	}
+	for i, list := range listA {
+		c, err := Mul(list, listB[i])
+		require.NoError(t, err)            // проверяем, что нет ошибки
+		require.Len(t, c, len(results[i])) // проверяем длину результата
+		// проверка значений
+		for j, v := range c {
+			require.Equal(t, results[i][j], v)
+		}
+	}
+	// проверяем на возврат ошибки
+	_, err := Mul([]int{22}, []int{33, 71})
+	require.Error(t, err)
+}
+
+func TestGCD(t *testing.T) {
+	pairs := [][]int{
+		{0, 5, 0},
+		{7, -350, 0},
+		{1460, 124, 4},
+		{198, 42, 6},
+		{1386, 4494, 42},
+		{1470, 1575, 105},
+	}
+	for _, pair := range pairs {
+		gcd := GCD(pair[0], pair[1])
+		if gcd != pair[2] {
+			t.Errorf("НОД(%d, %d): %d != %d\n", pair[0], pair[1], gcd, pair[2])
+		}
 	}
 }
