f3b8a06c7e21d2262b408f59c3bc2610d32a3390
Eduard <stedi.str@gmail.com>
2025-11-28 13:00:45 +0300

    v3.5.1 os, filepath test

--- sprint6/filePath/files/main.go
+++ sprint6/filePath/files/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func FileInfo(path string) {
@@ -22,9 +23,49 @@ func FileInfo(path string) {
 	fmt.Println("Время:", fileInfo.ModTime().Format("02.01.06 15:04:05"))
 }
 
+func DirInfo(path, shift string) error {
+	// получаем список файлов и поддиректорий
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		fi, err := file.Info()
+		if err != nil {
+			return err
+		}
+		if file.IsDir() {
+			fmt.Printf("%s%s\n", shift, file.Name())
+			// заходим внутрь директории
+			err = DirInfo(filepath.Join(path, file.Name()), shift+"  ")
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		// выводим информацию о файле
+		fmt.Printf("%s%s %s %d\n", shift, file.Name(),
+			fi.ModTime().Format("02.01.06 15:04:05"), fi.Size())
+	}
+	return nil
+}
+
 func main() {
 	// подставим несуществующий файл
 	FileInfo("file.go")
 	// проверим файл main.go
 	FileInfo("main.go")
+
+	fmt.Println()
+	// меняем текущую директорию
+	err := os.Chdir("../..")
+	// получаем новую текущую директорию
+	curDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Директория:", curDir)
+	if err = DirInfo(curDir, ""); err != nil {
+		log.Fatal(err)
+	}
 }
