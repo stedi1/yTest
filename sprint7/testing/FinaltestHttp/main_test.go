1ed3a8a0f61f00b8167b7f40eaca2fd9e0cb3093
Eduard <stedi.str@gmail.com>
2025-12-11 13:51:03 +0300

    v3.7.5

---
+++ sprint7/testing/FinaltestHttp/main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCafeWhenOk(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []string{
+		"/cafe?count=2&city=moscow",
+		"/cafe?city=tula",
+		"/cafe?city=moscow&search=ложка",
+	}
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v, nil)
+
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+		// пока сравнивать не будем, а просто выведем ответы
+		// удалите потом этот вывод
+		fmt.Println(response.Body.String())
+	}
+}
+
+func TestCafeWhenErr(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		request string
+		status  int
+		message string
+	}{
+		{"/cafe", http.StatusBadRequest, "unknown city"},
+		{"/cafe?city=omsk", http.StatusBadRequest, "unknown city"},
+		{"/cafe?city=tula&count=na", http.StatusBadRequest, "incorrect count"},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", v.request, nil)
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, v.status, response.Code)
+		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+	}
+}
---
+++ sprint7/testing/testHttpClient/go.mod
@@ -0,0 +1,3 @@
+module testhttpclient
+
+go 1.25.3
