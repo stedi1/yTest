a28d53d2ceb4df41f233d509c3f3ffab0a8005b5
Eduard <stedi.str@gmail.com>
2026-01-28 19:11:43 +0300

    v3.9.4

--- sprint9/mut1/main.go
+++ sprint9/mut1/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu sync.RWMutex
+	m  map[int]int
+}
+
+func (cache *Cache) Get(i int) int {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	v, ok := cache.m[i]
+	if ok {
+		return v
+	}
+	// получаем значение для указанного ключа
+	v = 2 * i
+	cache.m[i] = v
+	return v
+}
+
+func main() {
+	cache := Cache{
+		m: make(map[int]int),
+	}
+	for i := 0; i < 20; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				cache.Get(j)
+			}
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Println(len(cache.m))
+}
+
 // пример использования sync.RWMutex
 /* type Map struct {
 	mu sync.RWMutex
